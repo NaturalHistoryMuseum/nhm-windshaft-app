6dd459bd65ea48fb43b3b51adfd452fd44bac673
Andy Allan <andy@gravitystorm.co.uk>
2014-01-14 10:11:17 +0000

    Initial commit of the server component, based on the windshaft demo server.

---
+++ server.js
@@ -0,0 +1,36 @@
+// Note, currently to run this server your table must have a column called the_geom_webmercator with SRID of 3857
+// to view the tiles, open ./viewer/index.html and set the fields
+//
+// If you want to get something running quickly, follow the instructions for a seed DB in test/windshaft.test.sql
+
+var Windshaft = require('windshaft');
+var _         = require('underscore');
+
+var config = {
+    base_url: '/database/:dbname/table/:table',
+    base_url_notable: '/database/:dbname',
+    grainstore: {
+                 datasource: {user:'postgres', host: '127.0.0.1', port: 5432},
+                 styles: {point: "#botany_all {marker-fill: #00FFFF;marker-opacity: 1;marker-width: 8;marker-line-color: white;marker-line-width: 3;marker-line-opacity: 0.9;marker-placement: point;marker-type: ellipse;marker-allow-overlap: true;}"},
+    }, //see grainstore npm for other options
+    redis: {host: '127.0.0.1', port: 6379},
+    enable_cors: true,
+    req2params: function(req, callback){
+
+        // no default interactivity. to enable specify the database column you'd like to interact with
+        req.params.interactivity = "_id";
+
+        // this is in case you want to test sql parameters eg ...png?sql=select * from my_table limit 10
+        req.params =  _.extend({}, req.params);
+        _.extend(req.params, req.query);
+
+        // send the finished req object on
+        callback(null,req);
+    }
+};
+
+// Initialize tile server on port 4000
+var ws = new Windshaft.Server(config);
+ws.listen(4000);
+
+console.log("map tiles are now being served out of: http://localhost:4000" + config.base_url + '/:z/:x/:y');
